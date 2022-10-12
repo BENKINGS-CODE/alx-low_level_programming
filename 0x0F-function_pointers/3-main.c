f11c5a79e1b37d5999fbb816495f6341720c6468
BENKINGS-CODE <ifeanyichukwubenjamin59@gmail.com>
2022-10-12 15:00:42 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,37 +1,48 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
-*main - Take the arguments in command line to perform simple operations
-*
-*@argc: Number of arguments in the command line, Argumet count
-*@argv: Values to the operations placesl Argument vector
-*Return: 0
-*/
-
-int main(int argc, char **argv)
+ * main - get_op_func has operators correlated with
+ * func signs and funcs from op_func
+ * if not 4 arguments, return Error & exit 98
+ * if op is null, return Error & exit 99
+ * if div or mod 0, return Error & exit 100
+ * run calc, input one, operator, input two = pointer res to get_op
+ * @argc: arguments
+ * @argv: double pointer to arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
+	int one, two, ans;
+	int (*res)(int, int);
+	char *get_op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1] != '\0' || (get_op_func)(argv[2]) == NULL)
+
+	one = atoi(argv[1]);
+	two = atoi(argv[3]);
+	get_op = argv[2];
+
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((argv[2][0] == '%' || argv[2][0] == '/') && argv[3][0] == '0')
+	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%i\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	res = get_op_func(get_op);
+	ans = res(one, two);
 
+	printf("%d\n", ans);
 	return (0);
-
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,37 +1,53 @@
-#include <stdio.h>
 #include "3-calc.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
-*main - Take the arguments in command line to perform simple operations
-*
-*@argc: Number of arguments in the command line, Argumet count
-*@argv: Values to the operations placesl Argument vector
-*Return: 0
-*/
-
-int main(int argc, char **argv)
+ * op_add - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_add(int a, int b)
 {
-
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	if (argv[2][1] != '\0' || (get_op_func)(argv[2]) == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	if ((argv[2][0] == '%' || argv[2][0] == '/') && argv[3][0] == '0')
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	printf("%i\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
-
-	return (0);
-
+	return (a + b);
+}
+/**
+ * op_sub - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
 }
